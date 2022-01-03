6d22a95b2b874fe9aa0878c4c3ba529af2349151
gitaShojaee <61989071+gitaShojaee@users.noreply.github.com>
2022-01-03 12:43:00 +0330

    Update urls.py

--- IEproject/urls.py
+++ IEproject/urls.py
@@ -22,6 +22,3 @@ urlpatterns = [
     path('ticketing/', include('ticketing.urls')),
 ]
 
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
