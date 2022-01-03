61e2febd9a0f40b91ad8cae906e7379e65049181
gita shojaee <shojaeegita1379@gmail.com>
2022-01-03 16:23:32 +0330

    view: add views related to showtime

--- ticketing/views.py
+++ ticketing/views.py
@@ -1,17 +1,21 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from ticketing.models import Movie, Cinema
+from ticketing.models import Movie, Cinema, ShowTime
+
 
 def home_page(request):
     movies = Movie.objects.order_by('-year').all()[:5]
     cinemas = Cinema.objects.all()
     context = {
         'movies': movies,
-        'cinemas' : cinemas,
+        'cinemas': cinemas,
     }
     return render(request, 'ticketing/homePage.html', context)
+
+
 def movie_list(request):
     movies = Movie.objects.all()
     context = {
@@ -42,3 +46,20 @@ def cinema_details(request, cinema_id):
         'cinema': cinema
     }
     return render(request, 'ticketing/cinema_details.html', context)
+
+
+def showtime_list(request):
+    showtimes = ShowTime.objects.order_by('start_time').all()
+    context = {
+        'showtimes': showtimes,
+    }
+    return render(request, 'ticketing/showtime_list.html', context)
+
+
+@login_required
+def showtime_details(request, showtime_id):
+    showtime = ShowTime.objects.get(pk=showtime_id)
+    context = {
+        'showtime': showtime
+    }
+    return render(request, 'ticketing/showtime_details.html', context)
