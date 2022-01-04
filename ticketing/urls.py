fd8d9d8a03f6bfb0c4276eef7d646ffdadba5284
Shakiba Ehteshami <shakiba.eht15@gmail.com>
2022-01-04 21:13:24 +0330

    add showtime list and details files

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('movie/details/<int:movie_id>/', views.movie_details, name='movie_details'),
     path('cinema/list/', views.cinema_list, name='cinema_list'),
     path('cinema/details/<int:cinema_id>/', views.cinema_details, name='cinema_details'),
+    path('showtime/list/', views.showtime_list, name='showtime_list'),
+    path('showtime/details/<int:showtime_id>/', views.showtime_details, name='showtime_details'),
 ]
