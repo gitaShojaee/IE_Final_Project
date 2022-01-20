dcffa85982bc1981107ccc210e37352945a83c4c
gita shojaee <shojaeegita1379@gmail.com>
2022-01-21 00:04:41 +0330

    ticketing: complete showTime model

--- ticketing/models.py
+++ ticketing/models.py
@@ -54,6 +54,61 @@ class ShowTime(models.Model):
     )
     status = models.IntegerField(choices=status_choices, default=SALE_NOT_STARTED)
 
+    def get_price_display(self):
+        return '{}'.format(self.price)
+
+    def is_full(self):
+        """
+        Returns True if all seats are sold
+        """
+        return self.free_seats == 0
+
+    def open_sale(self):
+        """
+        Opens ticket sale
+        If sale was opened before, raises an exception
+        """
+        if self.status == ShowTime.SALE_NOT_STARTED:
+            self.status = ShowTime.SALE_OPEN
+            self.save()
+        else:
+            raise Exception('Sale has been started before')
+
+    def close_sale(self):
+        """
+        Closes ticket sale
+        If sale is not open, raises an exception
+        """
+        if self.status == ShowTime.SALE_OPEN:
+            self.status = ShowTime.SALE_CLOSED
+            self.save()
+        else:
+            raise Exception('Sale is not open')
+
+    def expire_showtime(self, is_canceled=False):
+        """
+        Expires showtime and updates the status
+        :param is_canceled: A boolean indicating whether the show is canceled or not, default is False
+        """
+        if self.status not in (ShowTime.MOVIE_PLAYED, ShowTime.SHOW_CANCELED):
+            self.status = ShowTime.SHOW_CANCELED if is_canceled else ShowTime.MOVIE_PLAYED
+            self.save()
+        else:
+            raise Exception('Show has been expired before')
+
+    def reserve_seats(self, seat_count):
+        """
+        Reserves one or more seats for a customer
+        :param seat_count: An integer as the number of seats to be reserved
+        """
+        assert isinstance(seat_count, int) and seat_count > 0, 'Number of seats should be a positive integer'
+        assert self.status == ShowTime.SALE_OPEN, 'Sale is not open'
+        assert self.free_seats >= seat_count, 'Not enough free seats'
+        self.free_seats -= seat_count
+        if self.free_seats == 0:
+            self.status = ShowTime.TICKETS_SOLD
+        self.save()
+
 
 class Ticket(models.Model):
     """
@@ -63,4 +118,3 @@ class Ticket(models.Model):
     customer = models.ForeignKey('accounts.Profile', on_delete=models.PROTECT)
     seat_count = models.IntegerField()
     order_time = models.DateTimeField(auto_now_add=True)
-
