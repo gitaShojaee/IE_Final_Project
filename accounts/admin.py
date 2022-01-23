9f34c4da211e5a8ea7780005b67a0a6cd05b8f2c
Shakiba Ehteshami <shakiba.eht15@gmail.com>
2022-01-24 00:50:46 +0330

    add payment list and form

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile
+from .models import Profile, Payment
 
 admin.site.register(Profile)
+admin.site.register(Payment)
